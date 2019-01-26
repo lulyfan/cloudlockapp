9be8f08f196968de08c34521be919fa2906b4e1b
chenjiajun <chenjiajun@ut.cn>
2019-01-26 10:30:03 +0800

    1.处理无网络，webview显示问题

--- base/src/main/java/com/ut/base/activity/WebActivity.java
+++ base/src/main/java/com/ut/base/activity/WebActivity.java
@@ -1,9 +1,16 @@
 package com.ut.base.activity;
 
 import android.databinding.DataBindingUtil;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
+import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.ut.base.BaseActivity;
@@ -19,16 +26,67 @@ import com.ut.base.databinding.ActivityWebBinding;
 
 @Route(path = RouterUtil.BaseModulePath.WEB)
 public class WebActivity extends BaseActivity {
+    private ActivityWebBinding binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityWebBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_web);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_web);
         initLightToolbar();
         String url = getIntent().getStringExtra("load_url");
         if (TextUtils.isEmpty(url)) {
             finish();
         }
         binding.webView.loadUrl(url);
+        binding.refreshLayout.setOnRefreshListener(() -> {
+            showErrorTips(false);
+            binding.webView.reload();
+            binding.refreshLayout.postDelayed(() -> {
+                binding.webView.reload();
+                binding.refreshLayout.setRefreshing(false);
+            }, 1200L);
+        });
+        webSettings();
+    }
+
+    private void webSettings() {
+        WebSettings settings = binding.webView.getSettings();
+        settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        WebViewClient client = new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                showErrorTips(false);
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                showErrorTips(true);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+
+                super.onReceivedError(view, errorCode, description, failingUrl);
+            }
+        };
+
+        binding.webView.setWebViewClient(client);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            binding.webView.setOnScrollChangeListener((v, scrollX, scrollY, oldScrollX, oldScrollY) -> {
+                if (binding.webView.getScrollY() == 0) {
+                    binding.refreshLayout.setEnabled(true);
+                } else {
+                    binding.refreshLayout.setEnabled(false);
+                }
+            });
+        }
+    }
+
+    private void showErrorTips(boolean isShow) {
+        binding.noNetworkLayout.setVisibility(isShow ? View.VISIBLE : View.GONE);
     }
 }
