a1b488f04dc8bfb95719d2b8b4bd381f0ac41181
zhouyubin <zhouyubin@ut.cn>
2019-01-26 15:40:12 +0800

    提交遗漏

---
+++ lib_networkapi/src/main/java/com/example/entity/entity/Cloudlockenterpriseinfo.java
@@ -0,0 +1,74 @@
+package com.example.entity.entity;
+
+/**
+ * author : zhouyubin
+ * time   : 2019/01/25
+ * desc   :
+ * version: 1.0
+ */
+public class Cloudlockenterpriseinfo {
+
+    /**
+     * mobileTitle : 客服电话
+     * mobile : 18125067770
+     * company : 公司官网
+     * url : www.zhunilink.com
+     * emailTitle : 官方邮箱
+     * email : smarthome@ut.cn
+     */
+
+    private String mobileTitle;
+    private String mobile;
+    private String company;
+    private String url;
+    private String emailTitle;
+    private String email;
+
+    public String getMobileTitle() {
+        return mobileTitle;
+    }
+
+    public void setMobileTitle(String mobileTitle) {
+        this.mobileTitle = mobileTitle;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getEmailTitle() {
+        return emailTitle;
+    }
+
+    public void setEmailTitle(String emailTitle) {
+        this.emailTitle = emailTitle;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ module_lock/src/main/java/com/ut/module_lock/common/LockTypeIcon.java
@@ -0,0 +1,10 @@
+package com.ut.module_lock.common;
+
+/**
+ * author : zhouyubin
+ * time   : 2019/01/24
+ * desc   :
+ * version: 1.0
+ */
+public class LockTypeIcon {
+}
