d30543f5c9f7316672b5d5bcedfbf4c2f791cd63
chenjiajun <chenjiajun@ut.cn>
2019-02-19 17:23:40 +0800

    修复删除分组后显示bug

--- module_lock/src/main/java/com/ut/module_lock/fragment/LockListFragment.java
+++ module_lock/src/main/java/com/ut/module_lock/fragment/LockListFragment.java
@@ -183,6 +183,7 @@ public class LockListFragment extends BaseFragment {
                     //如果之前选的分组已删除，则需要页面默认全部分组查询
                     if(hasDeleted) {
                         mLockListFragVM.getGroupLockList(allGroup);
+                        ((TextView) popupWindow.getView(R.id.lock_tv_group)).setText(allGroup.getName());
                     }
                 }
 
